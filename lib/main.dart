9d76bc3f1c3aef9a7b40dc24d1c18b1f4310387e
tuudug <tuudugprogrammer@gmail.com>
2023-05-23 18:23:09 +0800

    firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:pinly/screens/phone_verify.dart';
 
+import 'firebase_options.dart';
 import 'widgets/circle.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
