9feba1b17a056f475763a86276d5def4e7d3ab1c
tuudug <tuudugprogrammer@gmail.com>
2023-05-23 18:08:54 +0800

    change color from purple to deepPurple

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(
         fontFamily: "Sk-Modernist",
-        primarySwatch: Colors.purple,
+        primarySwatch: Colors.deepPurple,
       ),
       home: const MyHomePage(),
     );
--- lib/screens/phone_verify.dart
+++ lib/screens/phone_verify.dart
@@ -18,58 +18,67 @@ class _MyMobilePageState extends State<MyMobilePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('My Mobile'),
+        iconTheme: const IconThemeData(
+          color: Colors.black, //change your color here
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(bottom: 16.0),
-              child: Text(
-                'My Mobile',
-                style: TextStyle(
-                  fontSize: 32.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Text(
-              'Please enter your valid phone number. We will send you a 4-digit code to verify your account.',
-              style: TextStyle(
-                fontSize: 16.0,
-                color: Colors.grey,
-              ),
-            ),
-            SizedBox(height: 32.0),
-            TextField(
-              controller: _phoneNumberController,
-              keyboardType: TextInputType.phone,
-              decoration: InputDecoration(
-                labelText: 'Phone Number',
-              ),
-            ),
-            SizedBox(height: 32.0),
-            ElevatedButton(
-              onPressed: () {
-                // Add your logic for the Continue button here
-              },
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16.0),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(32, 168, 32, 32),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(bottom: 8.0),
                 child: Text(
-                  'Continue',
+                  'My Mobile',
                   style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
+                    fontSize: 32.0,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
               ),
-              style: ElevatedButton.styleFrom(
-                primary: Colors.purple,
+              const Text(
+                'Please enter your valid phone number. We will send you a 4-digit code to verify your account.',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  color: Colors.grey,
+                ),
               ),
-            ),
-          ],
+              const SizedBox(height: 64.0),
+              TextField(
+                controller: _phoneNumberController,
+                keyboardType: TextInputType.phone,
+                decoration: const InputDecoration(
+                  labelText: 'Phone Number',
+                ),
+              ),
+              const SizedBox(height: 32.0),
+              ElevatedButton(
+                onPressed: () {
+                  // Add your logic for the Continue button here
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.deepPurple,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16.0),
+                  ),
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 16.0),
+                  child: Text(
+                    'Continue',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
