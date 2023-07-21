e45e94ff0ddbb80a46e446023982adff34408ec2
tuudug <tuudugprogrammer@gmail.com>
2023-07-21 21:30:19 +0800

    some toolbar changes

--- lib/widgets/main_map_toolbar.dart
+++ lib/widgets/main_map_toolbar.dart
@@ -17,6 +17,8 @@ class MainMapToolbarState extends ConsumerState<MainMapToolbar> {
 
   @override
   Widget build(BuildContext context) {
+    int selectedPlaceType = ref.watch(selectedPlaceTypeProvider);
+
     return Padding(
       padding: const EdgeInsets.fromLTRB(8, 4, 8, 0),
       child: Column(
@@ -36,7 +38,10 @@ class MainMapToolbarState extends ConsumerState<MainMapToolbar> {
                             : morePlacesToggled = true;
                       });
                     },
-                    icon: Icon(Icons.menu_open),
+                    icon: AnimatedRotation(
+                        turns: morePlacesToggled ? 0.75 : 0,
+                        duration: Duration(milliseconds: 150),
+                        child: Icon(Icons.menu_open)),
                     style: IconButton.styleFrom(
                       backgroundColor:
                           morePlacesToggled ? pinlyPurple : Colors.white,
@@ -56,30 +61,18 @@ class MainMapToolbarState extends ConsumerState<MainMapToolbar> {
                 child: Padding(
                   padding: const EdgeInsets.only(right: 8),
                   child: Container(
-                    height: 38,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(17.0),
-                      border: Border.all(
-                        color: pinlyPurple,
-                        width: 1.0,
+                      height: 38,
+                      decoration: BoxDecoration(
+                        color: selectedPlaceType == -1
+                            ? Colors.white
+                            : Colors.deepPurple.withOpacity(0.9),
+                        borderRadius: BorderRadius.circular(17.0),
+                        border: Border.all(
+                          color: pinlyPurple,
+                          width: 1.0,
+                        ),
                       ),
-                    ),
-                    child: Row(
-                      children: [
-                        SizedBox(width: 8.0),
-                        Icon(
-                          Icons.search,
-                          color: Colors.grey,
-                        ),
-                        SizedBox(width: 8.0),
-                        Text(
-                          'Find a Place',
-                          style: TextStyle(color: Colors.grey),
-                        ),
-                      ],
-                    ),
-                  ),
+                      child: buildSearchBar(selectedPlaceType)),
                 ),
               ),
               Expanded(
@@ -135,25 +128,25 @@ class MainMapToolbarState extends ConsumerState<MainMapToolbar> {
                     child: PlaceTypeButton(
                       icon: Icons.restaurant,
                       text: "Restaurant",
-                      id: 1,
+                      id: 0,
                     )),
                 Expanded(
                     flex: 1,
                     child: PlaceTypeButton(
-                        icon: Icons.weekend, text: "Pub & Lounge", id: 2)),
+                        icon: Icons.weekend, text: "Pub & Lounge", id: 1)),
                 Expanded(
                     flex: 1,
                     child: PlaceTypeButton(
                       icon: Icons.coffee,
                       text: "Coffee Shop",
-                      id: 3,
+                      id: 2,
                     )),
                 Expanded(
                     flex: 1,
                     child: PlaceTypeButton(
                       icon: Icons.gamepad,
                       text: "Play",
-                      id: 4,
+                      id: 3,
                     )),
               ],
             ),
@@ -164,7 +157,7 @@ class MainMapToolbarState extends ConsumerState<MainMapToolbar> {
   }
 }
 
-class PlaceTypeButton extends StatelessWidget {
+class PlaceTypeButton extends ConsumerWidget {
   final IconData icon;
   final String text;
   final int id;
@@ -173,11 +166,13 @@ class PlaceTypeButton extends StatelessWidget {
       {super.key, required this.icon, required this.text, required this.id});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Padding(
       padding: const EdgeInsets.only(right: 8),
       child: TextButton.icon(
-          onPressed: () {},
+          onPressed: () {
+            ref.read(selectedPlaceTypeProvider.notifier).state = id;
+          },
           icon: Icon(
             icon,
             color: Colors.black,
@@ -199,4 +194,69 @@ class PlaceTypeButton extends StatelessWidget {
           )),
     );
   }
+}
+
+class PlaceTypeSelectedIndicator extends ConsumerWidget {
+  final IconData icon;
+  final String text;
+  final int id;
+
+  const PlaceTypeSelectedIndicator(
+      {super.key, required this.icon, required this.text, required this.id});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          children: [
+            SizedBox(width: 8.0),
+            Icon(
+              icon,
+              color: id == -1 ? Colors.black : Colors.white,
+            ),
+            SizedBox(width: 8.0),
+            Text(
+              text,
+              style: TextStyle(color: id == -1 ? Colors.black : Colors.white),
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            GestureDetector(
+              onTap: () {
+                ref.read(selectedPlaceTypeProvider.notifier).state = -1;
+              },
+              child: Icon(
+                Icons.close,
+                color: id == -1 ? Colors.white.withOpacity(0) : Colors.white,
+              ),
+            ),
+            SizedBox(width: 8.0),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+Widget buildSearchBar(int placeType) {
+  if (placeType == -1) {
+    return PlaceTypeSelectedIndicator(
+        icon: Icons.search, text: "Find a place", id: -1);
+  } else if (placeType == 0) {
+    return PlaceTypeSelectedIndicator(
+        icon: Icons.restaurant, text: "Restaurant", id: 0);
+  } else if (placeType == 1) {
+    return PlaceTypeSelectedIndicator(
+        icon: Icons.weekend, text: "Pub & Lounge", id: 1);
+  } else if (placeType == 2) {
+    return PlaceTypeSelectedIndicator(
+        icon: Icons.coffee, text: "Coffee Shop", id: 2);
+  } else if (placeType == 3) {
+    return PlaceTypeSelectedIndicator(icon: Icons.gamepad, text: "Play", id: 3);
+  }
+  return Text("Lol");
 }
